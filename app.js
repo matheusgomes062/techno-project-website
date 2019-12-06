5b97e40f944e64dcd46426a3c9ffdc99eedafd39
Matheus Gomes <matheusgomes062@gmail.com>
2019-12-06 20:34:36 -0300

    Importancao inicial, metade do projeto inicial feito

---
+++ app.js
@@ -0,0 +1,34 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+      })
+    }
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r;
+        })
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.produto = r;
+        })
+    }
+  },
+  created() {
+    this.fetchProdutos();
+  }
+})
---
+++ components/mainBody/mainBody.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="products">
+    <div v-for="product in products" :key="produto.id" class="procut">
+      <img :src="product.img" :alt="produto.nome" class="produto_img" />
+      <div>
+        <span class="product_price">{{product.price}}</span>
+        <h2 class="product_title">{{product.name}}</h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "main",
+  props: [],
+  data() {
+    return {};
+  }
+};
+</script>
