18ccb5139224c065b5b103a773e1386be550b19f
Matheus Gomes <matheusgomes062@gmail.com>
2019-12-07 06:39:31 -0300

    carrinho novamente

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const vm = new Vue({
   data: {
     produtos: [],
     produto: false,
+    carrinho: [],
   },
   filters: {
     numeroPreco(valor) {
       return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+    }
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        })
+      }
+      return total;
     }
   },
   methods: {
@@ -33,6 +45,14 @@ const vm = new Vue({
     },
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false;
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
     }
   },
   created() {
