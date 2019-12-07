5e7bd9792e701591918e7de66ad270dcc7e41935
Matheus Gomes <matheusgomes062@gmail.com>
2019-12-07 06:36:50 -0300

    carrinho

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
-    produto: false
+    produto: false,
   },
   filters: {
     numeroPreco(valor) {
-      return valor.toLocaleString("pt-BR", {
-        style: "currency",
-        currency: "BRL"
-      })
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
     }
   },
   methods: {
@@ -26,6 +23,16 @@ const vm = new Vue({
         .then(r => {
           this.produto = r;
         })
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
     }
   },
   created() {
