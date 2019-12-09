723b68f8695ef2ca33fb78b5426d315d7072ece3
matheuspaula <matheus.paula@agrotools.com.br>
2019-12-09 11:35:10 -0300

    correção do último commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: false,
   },
   filters: {
     numeroPreco(valor) {
@@ -50,6 +52,7 @@ const vm = new Vue({
       this.produto.estoque--;
       const { id, nome, preco } = this.produto;
       this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho.`);
     },
     removerItem(index) {
       this.carrinho.splice(index, 1);
@@ -57,6 +60,13 @@ const vm = new Vue({
     checarLocalStorage() {
       if (window.localStorage.carrinho)
         this.carrinho = JSON.parse(window.localStorage.carrinho);
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      }, 1500);
     }
   },
   watch: {
